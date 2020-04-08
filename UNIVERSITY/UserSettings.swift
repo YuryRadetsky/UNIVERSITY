03be5ae142f31209bb0d16ebdf932c6b12af30b5
Yury Radetsky <yura.radetsky@gmail.com>
2020-04-08 20:16:52 +0300

    add UserSettings

---
+++ UNIVERSITY/UserSettings.swift
@@ -0,0 +1,54 @@
+//
+//  UserSettings.swift
+//  UNIVERSITY
+//
+//  Created by Yury Radetsky on 08.04.2020.
+//  Copyright © 2020 YuryRadetsky. All rights reserved.
+//
+
+import Foundation
+
+final class UserSettings {
+    // приватный enum, который будет хранить ключи
+    private enum SettingsKeys: String {
+        case userName
+        case password
+    }
+    
+    static var userName: String! {
+        get {
+            // достаем значение из UserDefaults 
+            return UserDefaults.standard.string(forKey: SettingsKeys.userName.rawValue)
+        } set {
+            let defaults = UserDefaults.standard
+            let key = SettingsKeys.userName.rawValue
+            // получаем новое значенеи 
+            if let username = newValue {
+                print("value: \(username) was added to key \(key)")
+                defaults.set(username, forKey: key)
+                
+            } else {
+                // если не получилось достать новое значение (nil) , тогда удаляем у объекта defaults по ключу key
+                defaults.removeObject(forKey: key)
+            }
+        }
+    }
+    
+    static var password: String! {
+        get {
+            // достаем значение из UserDefaults
+            return UserDefaults.standard.string(forKey: SettingsKeys.password.rawValue)
+        } set {
+            let defaults = UserDefaults.standard
+            let key = SettingsKeys.password.rawValue
+            if let password = newValue {
+                print("value: \(password) was added to key \(key)")
+                defaults.set(password, forKey: key)
+            } else {
+                // если не получилось достать новое значение (nil) , тогда удаляем у объекта defaults по ключу key
+                defaults.removeObject(forKey: key)
+            }
+        }
+    }
+    
+}
--- UNIVERSITY/ViewController.swift
+++ UNIVERSITY/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    
+    @IBAction func usernameTextField(_ sender: Any) {
+        print(username.text)
+    }
+    @IBAction func passwordTextField(_ sender: Any) {
+        print(password.text)
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        // функция trimmingCharacters удаляет пустые пространства
+        let usernameTrimmingText = username.text?.trimmingCharacters(in: .whitespaces)
+        
+        UserSettings.userName = usernameTrimmingText
+        print(UserSettings.userName)
+        
+        let passwordText = password.text
+        UserSettings.password = passwordText
+        print(UserSettings.password)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
