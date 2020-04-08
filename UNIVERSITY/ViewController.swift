899a80322eb23fc899cd53790578f158b4725416
Yury Radetsky <yura.radetsky@gmail.com>
2020-04-08 15:58:26 +0300

    first commit

---
+++ UNIVERSITY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UNIVERSITY
+//
+//  Created by Yury Radetsky on 08.04.2020.
+//  Copyright © 2020 YuryRadetsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
